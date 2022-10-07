3f94864ce006062fdc3db19c6a3fd53f61d346d5
Robson Barbosa Souza <powerrob92@gmail.com>
2022-10-07 10:21:49 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -24,9 +24,9 @@ copyright = u'2022, TED-SAP/IFG'
 author = u'TED-SAP/IFG'
 
 # The short X.Y version
-version = u'0.1'
+version = u'1.0'
 # The full version, including alpha/beta/rc tags
-release = u'0.1.0'
+release = u'1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
