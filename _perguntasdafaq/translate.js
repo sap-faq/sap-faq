ba79a421a043bd386b74ca2b7ea2b088c41ffcbc
Robson Barbosa Souza <robson.souza@olgari.com.br>
2022-10-09 14:37:26 -0300

    translate butons

---
+++ _perguntasdafaq/translate.js
@@ -0,0 +1,4 @@
+$next_btn = document.querySelector('.btn.btn-neutral.float-right');
+$next_btn.innerHTML =$next_btn.innerHTML.replace('Next','Próximo');
+$prev_btn = document.querySelector('.btn.btn-neutral.float-left');
+$prev_btn .innerHTML =$prev_btn.innerHTML.replace('Previous','Anterior');
